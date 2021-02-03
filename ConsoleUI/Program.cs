eb72d04db936195fa8b51c60685f1bd339ebfee2
Fatih Çil <fatihcil.2001@gmail.com>
2021-02-03 16:07:56 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,33 +11,30 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             var carManager = new CarManager(new InMemoryProductDal());
-            Write(carManager,"Default");
+            Write(carManager.GetAll(), "Default");
             //Add Test
             carManager.Add(new Car { CarId = 6, BrandId = 1, ColorId = 2, DailyPrice = 50000, Description = "NEW CAR", ModelYear = 2021 });
-            Write(carManager,"Added");
+            Write(carManager.GetAll(), "Added");
 
 
             //Delete Test
             carManager.Delete(new Car { CarId = 6 });
-            Write(carManager,"Deleted");
+            Write(carManager.GetAll(), "Deleted");
 
             //Update Test
             carManager.Update(new Car { CarId = 5, BrandId = 5, ColorId = 3, DailyPrice = 70000, Description = "UPDATED CAR", ModelYear = 2021 });
-            Write(carManager,"Updated");
+            Write(carManager.GetAll(), "Updated");
 
             //GetByID Test
-            Console.WriteLine("------------------------------GetByID--------------------------------------------------");
-            foreach (var car in carManager.GetById(3))
-            {
-                Console.WriteLine("CarID: {0} MarkaID: {1} ColorId: {2} DailyPrice: {3} Description: {4} ModelYear: {5}", car.CarId, car.BrandId, car.ColorId, car.DailyPrice, car.Description, car.ModelYear);
-            }
+            Write(carManager.GetById(3), "GetByID");
+
         }
 
 
-        static void Write(CarManager carManager, string Msg)
+        static void Write(List<Car> carList, string Msg)
         {
             Console.WriteLine("------------------------------{0}--------------------------------------------------", Msg);
-            foreach (var car in carManager.GetAll())
+            foreach (var car in carList)
             {
                 Console.WriteLine("CarID: {0} MarkaID: {1} ColorId: {2} DailyPrice: {3} Description: {4} ModelYear: {5}", car.CarId, car.BrandId, car.ColorId, car.DailyPrice, car.Description, car.ModelYear);
             }
